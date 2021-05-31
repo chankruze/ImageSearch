65f528763f06abdc7fdbdcfcdf439071412b003c
chankruze <chankruze@gmail.com>
2021-05-31 22:26:42 +0530

    Prepare Gallery UI & Navigation
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/src/main/java/in/geekofia/imagesearch/data/UnsplashPhoto.kt
@@ -0,0 +1,31 @@
+package `in`.geekofia.imagesearch.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+// implement parcelable
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String,
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val id: String,
+        val username: String,
+        val name: String,
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearch&utm_medium=referral"
+    }
+}
---
+++ app/src/main/java/in/geekofia/imagesearch/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package `in`.geekofia.imagesearch.ui.gallery
+
+import `in`.geekofia.imagesearch.R
+import androidx.fragment.app.Fragment
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+}
