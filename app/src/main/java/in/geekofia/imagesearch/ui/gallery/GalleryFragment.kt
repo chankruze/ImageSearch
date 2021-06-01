5835bc6c4cff337e75ad6a76439ecca2c7f0a357
chankruze <chankruze@gmail.com>
2021-06-01 11:20:08 +0530

    Implement Paging Data Adapter & View Binding
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/in/geekofia/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/in/geekofia/imagesearch/ui/gallery/GalleryFragment.kt
@@ -1,6 +1,7 @@
 package `in`.geekofia.imagesearch.ui.gallery
 
 import `in`.geekofia.imagesearch.R
+import `in`.geekofia.imagesearch.databinding.FragmentGalleryBinding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -11,11 +12,28 @@ import dagger.hilt.android.AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
-            TODO("Not implemented yet")
+        _binding = FragmentGalleryBinding.bind(view)
+
+        val adapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
